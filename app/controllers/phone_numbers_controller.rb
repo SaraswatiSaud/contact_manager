da75e72cf8f95a7ea535caf352bf95f8fa64fb28
Saraswati Saud <saraswati.saud@gmail.com>
2017-08-11 17:12:37 +0545

    Added edit feature to phone number

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -5,6 +5,27 @@ class PhoneNumbersController < ApplicationController
     redirect_to person_path(@person)
   end
 
+  def edit
+    @person = Person.find(params[:person_id])
+    @phone_number = @person.phone_numbers.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:person_id])
+    @phone_number = @person.phone_numbers.find(params[:id])
+
+    if @phone_number.update(phone_number_params)
+      redirect_to @person
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @person = Person.find(params[:person_id])
+    @phone_number = PhoneNumber.find(params[:id])
+  end
+
   def destroy
     @person = Person.find(params[:person_id])
     @phone_number = PhoneNumber.find(params[:id])
