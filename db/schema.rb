b3225fcaa38460a1a3b03cd0c7308a301c30f87e
Saraswati Saud <saraswati.saud@gmail.com>
2017-08-13 12:48:00 +0545

    Authorization to a particular user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813045139) do
+ActiveRecord::Schema.define(version: 20170813064353) do
 
   create_table "emails", force: :cascade do |t|
     t.string "address"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170813045139) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_people_on_user_id"
   end
 
   create_table "phone_numbers", force: :cascade do |t|
